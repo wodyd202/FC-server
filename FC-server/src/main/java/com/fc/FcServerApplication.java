bea5259bc77f08c504dabb51a1ce42056755ee5e
wodyd202 <wodyd202@naver.com>
2021-05-25 12:09:14 +0900

    start project

---
+++ FC-server/src/main/java/com/fc/FcServerApplication.java
@@ -0,0 +1,13 @@
+package com.fc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcServerApplication.class, args);
+	}
+
+}
